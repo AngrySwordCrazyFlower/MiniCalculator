5e2aed102cddcbfe54897dfbad2460021669a19c
Woo <1310178049@qq.com>
2018-09-26 19:33:20 +0800

    add suffix convert machine

--- src/SuffixConvertMachine.java
+++ src/SuffixConvertMachine.java
@@ -1,18 +1,62 @@
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Stack;
 public class SuffixConvertMachine {
+    public final static HashMap<Token.TokenType,Integer> priorityTable = null;
+
+    static{
+        priorityTable.put(Token.TokenType.OPERATION_PLUS,10);
+        priorityTable.put(Token.TokenType.OPERATION_MINUS,10);
+        priorityTable.put(Token.TokenType.OPERATION_MULTIPLY,20);
+        priorityTable.put(Token.TokenType.OPERATION_DIVIDE,20);
+        priorityTable.put(Token.TokenType.OPERATION_LEFT_BRACKET,30);
+        priorityTable.put(Token.TokenType.OPERATION_RIGHT_BRACKET,30);
+    }
+
+    public static int getPriority(Token token){
+        return priorityTable.get(token.getTokenType());
+    }
 
     public static ArrayList<Token> Convert(ArrayList<Token> infixTokens) {
         ArrayList<Token> suffix = new ArrayList<>();
         Stack<Token> stk = new Stack<>();
         for (Token token : infixTokens) {
-            if (token.getTokenType() == Token.TokenType.OPERATION) {
-                stk.push(token);
-            } else if ()
+            Token topToken = stk.peek();
+            if(topToken != null){
+                if (token.getTokenType() == Token.TokenType.OPERATION_PLUS ||
+                        token.getTokenType() == Token.TokenType.OPERATION_MINUS ||
+                        token.getTokenType() == Token.TokenType.OPERATION_MULTIPLY ||
+                        token.getTokenType() == Token.TokenType.OPERATION_DIVIDE){
+                    if(getPriority(topToken) < getPriority(token))
+                        stk.push(token);
+                    else{
+                        if(stk.peek().getTokenType() != Token.TokenType.OPERATION_LEFT_BRACKET){
+                            suffix.add(stk.peek());
+                        }
+                        stk.pop();
+                        stk.push(token);
+                    }
+                }
+                else if(token.getTokenType() == Token.TokenType.OPERATION_LEFT_BRACKET){
+                    stk.push(token);
+                }
+                else if(token.getTokenType() == Token.TokenType.OPERATION_RIGHT_BRACKET){
+                    while(stk.peek().getTokenType() != Token.TokenType.OPERATION_LEFT_BRACKET){
+                        suffix.add(stk.peek());
+                        stk.pop();
+                    }
+                }
+                else if (token.getTokenType() == Token.TokenType.INTEGER ||
+                        token.getTokenType() == Token.TokenType.DOUBLE ||
+                        token.getTokenType() == Token.TokenType.VARIABLE){
+                    suffix.add(token);
+                }
+            }
+
         }
 
-        return null;
+        return suffix;
     }
 
 }
--- src/Test.java
+++ src/Test.java
@@ -6,16 +6,19 @@ public class Test {
     public static void main(String[] args) {
         try {
 
-            TokenScanner scanner = new TokenScanner("a+2 * 3.1", CustomVariableTable.GLOBAL_VARIABLE_TABLE);
+            TokenScanner scanner = new TokenScanner("a + b*c + (d * e + f) * g", CustomVariableTable.GLOBAL_VARIABLE_TABLE);
 
             ArrayList<Token> tokens = scanner.getTokens();
+
             for (Token token : tokens) {
                 System.out.println(token.getName());
             }
 
-            int a = 1;
-            int b = 2;
-            System.out.println(a + + + - - - + + + b);
+            ArrayList<Token> suffixTokens = SuffixConvertMachine.Convert(tokens);
+            for (Token token : suffixTokens) {
+                System.out.println(token.getName());
+            }
+
         } catch (MyException e) {
             e.printStackTrace();
         }
